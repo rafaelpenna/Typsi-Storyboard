861471c8cdf9f079bed8cdfab13d474b56b56f13
Rafael Penna <ragapen@gmail.com>
2023-03-30 07:39:04 -0300

    initial commit

---
+++ Typsi-Storyboard/Typsi-StoryboardUITests/Typsi_StoryboardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Typsi_StoryboardUITestsLaunchTests.swift
+//  Typsi-StoryboardUITests
+//
+//  Created by Rafael Penna on 30/03/23.
+//
+
+import XCTest
+
+class Typsi_StoryboardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
