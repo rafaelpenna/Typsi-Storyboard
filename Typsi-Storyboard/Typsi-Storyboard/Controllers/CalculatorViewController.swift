105115c6654424bbac3e1d907bc5d8e86e585848
Rafael Penna <ragapen@gmail.com>
2023-03-30 11:36:16 -0300

    IBOutlets and IBActions were set, conditional for the tips button were defined and the Calculate button is printing the amount of tips

--- Typsi-Storyboard/Typsi-Storyboard/Controllers/CalculatorViewController.swift
+++ Typsi-Storyboard/Typsi-Storyboard/Controllers/CalculatorViewController.swift
@@ -18,12 +18,32 @@ class CalculatorViewController: UIViewController {
 
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        if zeroPctButton.isTouchInside == true {
+            zeroPctButton.isSelected = true
+            tenPctButton.isSelected = false
+            twentyPctButton.isSelected = false
+        } else if tenPctButton.isTouchInside == true {
+            zeroPctButton.isSelected = false
+            tenPctButton.isSelected = true
+            twentyPctButton.isSelected = false
+        } else {
+            zeroPctButton.isSelected = false
+            tenPctButton.isSelected = false
+            twentyPctButton.isSelected = true
+        }
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     
     @IBAction func calculatedPressed(_ sender: UIButton) {
+        if zeroPctButton.isSelected == true {
+            print(0.0)
+        } else if tenPctButton.isSelected == true {
+            print(0.1)
+        } else {
+            print(0.2)
+        }
     }
     
     
--- Typsi-Storyboard/Typsi-Storyboard/Controllers/ResultsViewController.swift
+++ Typsi-Storyboard/Typsi-Storyboard/Controllers/ResultsViewController.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet var totalLabel: UILabel!
+    @IBOutlet var settingsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func recalculatePressed(_ sender: UIButton) {
     }
-    */
-
+    
 }
