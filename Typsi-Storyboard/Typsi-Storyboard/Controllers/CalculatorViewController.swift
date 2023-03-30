006c848549e4431174f2172844be2b755228c1ea
Rafael Penna <ragapen@gmail.com>
2023-03-30 11:08:15 -0300

    conclusion of storyboard layout configuration

---
+++ Typsi-Storyboard/Typsi-Storyboard/Controllers/CalculatorViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Typsi-Storyboard
+//
+//  Created by Rafael Penna on 30/03/23.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet var billTextField: UITextField!
+    @IBOutlet var zeroPctButton: UIButton!
+    @IBOutlet var tenPctButton: UIButton!
+    @IBOutlet var twentyPctButton: UIButton!
+    @IBOutlet var splitBillLabel: UILabel!
+    
+
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    }
+    
+    @IBAction func calculatedPressed(_ sender: UIButton) {
+    }
+    
+    
+}
+
